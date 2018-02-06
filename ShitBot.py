428e770b3beb55b56385ee069d3ea34e64245821
Brady Shea <Bradford97@live.com>
2018-02-06 15:40:08 -0500

    reverrrrrt

--- ShitBot.py
+++ ShitBot.py
@@ -1,127 +1,22 @@
 import discord
-from discord.ext import commands
+import asyncio
 import random
 from quote import quotes
-from pokemon import pokemon
 
 client = discord.Client()
-bot = commands.Bot(command_prefix='%')
 
-@bot.event
+@client.event
 async def on_ready():
     print('Logged in as')
-    print(bot.user)
+    print(client.user.name)
+    print(client.user.id)
     print('------')
-    await bot.change_presence(game=discord.Game(name='%quote'))
+    await client.change_presence(game=discord.Game(name='%quote'))
 
-@bot.command()
-async def quote(ctx):
-    x = random.choice(list(quotes.keys()))
-    await ctx.send('"' + x + '" - ' + quotes[x])
+@client.event
+async def on_message(message):
+    if message.content.startswith('%quote'):
+        x = random.choice(list(quotes.keys()))
+        await client.send_message(message.channel, '"' + x + '" - ' + quotes[x])
 
-
-## This section contains all of the code for sprites.
-
-@bot.command()
-async def pmd(ctx, arg):
-    if (int(arg) < 1 or int(arg) > 807):
-        await ctx.send("Index out of bounds.")
-        return
-    elif (int(arg) == 721):
-        await ctx.send("Volcanion has not appeared in a PMD game.")
-        return
-    elif (int(arg) > 721 and int(arg) < 808):
-        await ctx.send("Gen VII Pokemon have not yet appeared in PMD games.")
-        return
-    elif (int(arg) < 10):
-        arg = "00" + str(arg)
-    elif (int(arg) < 100):
-        arg = "0" + str(arg)
-    x = "https://serebii.net/supermysterydungeon/pokemon/" + arg + ".png"
-    y = discord.Embed()
-    y.set_image(url=x)
-    await ctx.send(embed = y)
-
-@bot.command()
-async def rb(ctx, arg):
-    x = "https://img.pokemondb.net/sprites/red-blue/normal/" + arg + ".png"
-    y = discord.Embed()
-    y.set_image(url=x)
-    if arg in pokemon:
-        if (int(pokemon[arg]) > 151):
-            await ctx.send("This Pokemon did not exist in Red/Blue.")
-            return
-        await ctx.send(embed = y)
-    else:
-        await ctx.send("Your input is either not a Pokemon or not yet added to the list of Pokemon.")
-
-@bot.command()
-async def yellow(ctx, arg):
-    x = "https://img.pokemondb.net/sprites/yellow/normal/" + arg + ".png"
-    y = discord.Embed()
-    y.set_image(url=x)
-    if arg in pokemon:
-        if (int(pokemon[arg]) > 151):
-            await ctx.send("This Pokemon did not exist in Yellow.")
-            return
-        await ctx.send(embed = y)
-    else:
-        await ctx.send("Your input is either not a Pokemon or not yet added to the list of Pokemon.")
-
-@bot.command()
-async def silver(ctx, arg):
-    x = "https://img.pokemondb.net/sprites/silver/normal/" + arg + ".png"
-    y = discord.Embed()
-    y.set_image(url=x)
-    await ctx.send(embed = y)
-
-@bot.command()
-async def gold(ctx, arg):
-    x = "https://img.pokemondb.net/sprites/gold/normal/" + arg + ".png"
-    y = discord.Embed()
-    y.set_image(url=x)
-    await ctx.send(embed = y)
-
-@bot.command()
-async def crystal(ctx, arg):
-    x = "https://img.pokemondb.net/sprites/crystal/normal/" + arg + ".png"
-    y = discord.Embed()
-    y.set_image(url=x)
-    await ctx.send(embed = y)
-
-@bot.command()
-async def rse(ctx, arg):
-    x = "https://img.pokemondb.net/sprites/ruby-sapphire/normal/" + arg + ".png"
-    y = discord.Embed()
-    y.set_image(url=x)
-    await ctx.send(embed = y)
-
-@bot.command()
-async def frlg(ctx, arg):
-    x = "https://img.pokemondb.net/sprites/firered-leafgreen/normal/" + arg + ".png"
-    y = discord.Embed()
-    y.set_image(url=x)
-    await ctx.send(embed = y)
-
-@bot.command()
-async def dppt(ctx, arg):
-    x = "https://img.pokemondb.net/sprites/diamond-pearl/normal/" + arg + ".png"
-    y = discord.Embed()
-    y.set_image(url=x)
-    await ctx.send(embed = y)
-
-@bot.command()
-async def hgss(ctx, arg):
-    x = "https://img.pokemondb.net/sprites/heartgold-soulsilver/normal/" + arg + ".png"
-    y = discord.Embed()
-    y.set_image(url=x)
-    await ctx.send(embed = y)
-
-@bot.command()
-async def bw(ctx, arg):
-    x = "https://img.pokemondb.net/sprites/black-white/anim/normal/" + arg + ".gif"
-    y = discord.Embed()
-    y.set_image(url=x)
-    await ctx.send(embed = y)
-
-bot.run('NDEwMjM1NjgxMTA2MDM0Njg5.DVqNRA.5hICSESXedjhaue_vwXYu0JqVDY')
+client.run('NDEwMjM1NjgxMTA2MDM0Njg5.DVqNRA.5hICSESXedjhaue_vwXYu0JqVDY')
