1a89ad47a4efb4706f5080aaf5d977d820c509a2
Brady Shea <Bradford97@live.com>
2018-02-26 16:16:02 -0500

    added pokemon and personal commands

--- ShitBot.py
+++ ShitBot.py
@@ -23,6 +23,7 @@ async def quote(ctx):
 async def echo(ctx, *, arg):
     await ctx.send(arg)
 
+
 ## This section contains all of the code for generating sprites.
 
 @bot.command()
@@ -144,6 +145,274 @@ async def hgss(ctx, arg):
 @bot.command()
 async def bw(ctx, arg):
     x = "https://img.pokemondb.net/sprites/black-white/anim/normal/" + arg + ".gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def xy(ctx, arg):
+    if arg in pokemon:
+        if (int(pokemon[arg]) > 721):
+            await ctx.send("This Pokemon did not exist in X/Y.")
+            return
+        await xysm(ctx, arg)
+    else:
+        await ctx.send("Your input is either not a Pokemon or did not exist in X/Y.")
+
+@bot.command()
+async def sm(ctx, arg):
+    await xysm(ctx, arg)
+
+async def xysm(ctx, arg):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/" + arg + ".gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+
+# The following section contains commands for each individual user
+
+@bot.command()
+async def sig(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/gliscor.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def flames(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/lopunny.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def diamond(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/glaceon.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def odd(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/slowpoke.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def hecc(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/gourgeist.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def fluffy(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/leafeon.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def surv(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/luxray.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def dom(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/charizard.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def psych(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/slaking.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def ama(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/amaura.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def bounty(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/porygonz.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def mako(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/primarina.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def timo(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/shaymin-sky.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def cell(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/gengar.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def dragon(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/latios.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def gex(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/treecko.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def tallow(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/zangoose.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def frogger(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/leafeon.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def alaska(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/lurantis.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def minty(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/archen.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def meth(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/mawile.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def hayden(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/infernape.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def nas(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/leafeon.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def nathan(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/goodra.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def zytomic(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/lurantis.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def testin(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/serperior.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def gary(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/gyarados.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def ivee(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/sylveon.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def patrician(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/bronzong.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def drag(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/sylveon.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def curtis(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/reuniclus.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def seymour(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/altaria.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def fooni(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/froslass.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def ace(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/suicune.gif"
+    y = discord.Embed()
+    y.set_image(url=x)
+    await ctx.send(embed = y)
+
+@bot.command()
+async def cones(ctx):
+    x = "https://play.pokemonshowdown.com/sprites/xyani/vanillite.gif"
     y = discord.Embed()
     y.set_image(url=x)
     await ctx.send(embed = y)
